a0cec91964c71a8281109319d91777bc5a0314ab
songxuexian <s.xue.xian.c@gmail.com>
2016-03-08 19:47:23 +0800

    Test code of action Sensor!

---
+++ app/src/main/java/com/xuexian/listsensor/MainActivity.java
@@ -0,0 +1,74 @@
+package com.xuexian.listsensor;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements SensorEventListener {
+
+    private TextView tvAccelerometer;
+    private SensorManager sensorManager;
+    private float[] gravity = new float[3];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvAccelerometer = (TextView) findViewById(R.id.show);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                sensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY),
+                sensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        switch (event.sensor.getType()) {
+            case Sensor.TYPE_ACCELEROMETER:
+                final float alpha = (float) 0.8;
+                gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+                gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+                gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
+
+                String accelerometer = "加速度\n" + "X:"
+                        + (event.values[0] - gravity[0]) + "\n"
+                        + "Y:" + (event.values[1] - gravity[1]) + "\n"
+                        + "Z:" + (event.values[2] - gravity[2]) + "\n";
+                Log.d("z",String.valueOf(event.values[2]-gravity[2]));
+                tvAccelerometer.setText(accelerometer);
+                break;
+            case Sensor.TYPE_GRAVITY:
+                gravity[0] = event.values[0];
+                gravity[1] = event.values[1];
+                gravity[2] = event.values[2];
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
